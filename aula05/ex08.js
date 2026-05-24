63d3f84de8e730b85b94b51584bcf886b3ccc50f
Gabriel Soares <gabrielabreusoares018@gmail.com>
2026-05-24 15:52:48 -0300

    Condições

---
+++ aula05/ex08.js
@@ -0,0 +1,8 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+} else{
+    console.log(`Você está dentro do limite de velocidade. Tenha um bom dia!`)
+}
